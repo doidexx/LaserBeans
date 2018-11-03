4919c13e324e21a4bafef4611ddbbc5a5348d2ec
doidexx <doidexx000@gmail.com>
2018-11-03 02:06:21 -0400

    first push

--- LaserBeans/Assets/Scripts/Player.cs
+++ LaserBeans/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
@@ -8,14 +9,19 @@ public class Player : MonoBehaviour {
 
 	public float exforce, exradius;
 	private Camera playerCamera;
-	public GameObject bullet, target1, dontKill1;
+	public GameObject bullet, target1, dontKill1, bulletImage, myCanvas, outofamo;
+	public Text ammoc;
 	public GameObject[] target;
     public GameObject[] dontKill;
+
+	private GameObject[] bulletImage1;
 	private float[] speed = new float [10];
 	public int ammo;
 	void Start () {
+		Cursor.visible = false;
 		target = new GameObject[10];
 		dontKill = new GameObject[10];
+		bulletImage1 = new GameObject[ammo];
 		playerCamera = GetComponent<Camera>();
 		for (int i = 0; i < 10; i++) {
 			if (i < 1){
@@ -27,7 +33,15 @@ public class Player : MonoBehaviour {
                 dontKill[i] = Instantiate(dontKill1, new Vector3(dontKill1.transform.position.x, dontKill1.transform.position.y, dontKill[i - 1].transform.position.z + 2), Quaternion.identity);
             	speed[i] = Random.Range(5, 15);
 			}
-		}
+        }
+        for (int i = 0; i < ammo; i++) {
+			if (i < 1) {
+                bulletImage1[i] = Instantiate(bulletImage, bulletImage.transform.position + (Vector3.down * 3f), Quaternion.identity);
+			} else {
+                bulletImage1[i] = Instantiate(bulletImage, bulletImage1[i - 1].transform.position + (Vector3.down * 3f), Quaternion.identity);
+			}
+            bulletImage1[i].transform.SetParent(myCanvas.transform);
+        }
 	}
 	
 	// Update is called once per frame
@@ -38,12 +52,24 @@ public class Player : MonoBehaviour {
         }
 		Ray laserBean = playerCamera.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hitPoint;
-		if (Physics.Raycast(laserBean, out hitPoint) && Input.GetMouseButtonDown(0) && ammo != 0) {
+		GetComponent<LineRenderer>().SetPosition(1, laserBean.direction * 1000f);
+		GetComponent<LineRenderer>().SetPosition(0, transform.position + (Vector3.down/2f));
+		if (Physics.Raycast(laserBean, out hitPoint)) {
 			Physics.Linecast(transform.position, Input.mousePosition);
+             if (Input.GetMouseButtonDown(0) && ammo != 0){
                 GameObject clone;
-				clone = Instantiate(bullet, transform.position, Quaternion.identity);
+				clone = Instantiate(bullet, transform.position + (Vector3.down/2), Quaternion.identity);
                 clone.GetComponent<Rigidbody>().AddForce(laserBean.direction * 40f, ForceMode.Impulse);
+                bulletImage1[ammo-1].SetActive(false);
 				ammo--;
+			}
 		}
+		if (ammo == 0) {
+			outofamo.SetActive(true);
+		}
+		else {
+			outofamo.SetActive(false);
+		}
+		ammoc.text = ammo.ToString();
 	}
 }
